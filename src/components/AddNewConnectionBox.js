2a707621414b525bb3c7c19070f1cbdb37f87dee
dio-p <dionempapadop@gmail.com>
2022-11-21 16:59:33 +0000

    working on the creation of the new dec object

--- src/components/AddNewConnectionBox.js
+++ src/components/AddNewConnectionBox.js
@@ -10,23 +10,27 @@ const AddingConnectionBox = styled.div`
     `;
 
 const FormContainer = styled.form`
-
-`;
-
-const UrlInputBox = styled.div`
+    margin: 6px;
 
 `;
 
 const InputContainer = styled.div`
+    margin-top: 4px;
     border: solid black;
-    border-width: 2px;
+    border-radius: 10px;
     padding: 1px 2px;
-    width: 200px;
+    min-width: 200px;
+    width: max-content;    ;
     min-height: 20px;
     height: 100%
+    border-radius: 15px 10%;
 `;
 
 const Input = styled.input`
+    border-radius: 8px;
+    min-width: 200px;
+    width: 100%; 
+    height: 24px;
     writing-mode: horizontal-tb !important;
     font-style: ;
     font-variant-ligatures: ;
@@ -46,25 +50,47 @@ const Input = styled.input`
     text-indent: 0px;
     text-shadow: none;
     display: inline-block;
-    text-align: start;
+    text-align: center;
     appearance: auto;
     -webkit-rtl-ordering: logical;
     cursor: text;
     background-color: field;
     margin: 0em;
-    padding: 1px 2px;
+    padding: 0px;
     border-width: 0px;
     border-style: inset;
     border-color: -internal-light-dark(rgb(118, 118, 118), rgb(133, 133, 133));
     border-image: initial;
 `;
 
+const UrlInputBox = styled.div`
+    text-align: center;
+    color: #b5b4ac;
+    cursor: pointer;
+    margin: 3px;
+`;
+
 const AddNewConnectionBox = () => {
     const [url, setUrl] = useState("");
+    const [newDoc, setNewDoc] = useState("");
 
     useEffect(() => {
         console.log("url", url);
     }, [url]);
+
+    const addhasBeenClicked = () => {
+        console.log("add has been clicked ");
+        const newDoc = {
+            title: "",
+            url: url,
+            source: "Confluence",
+            creationDate: "someDate",
+            folderToBeDisplayedIn: "folder1",
+            concerningParts: [],
+            isLinkUpToDate: true, //tickbox checked
+        }
+        setNewDoc(newDoc)
+    }
 
     // const onChangeFired = useCallback((e) => {
     //     setUrl(e.target.value) 
@@ -79,7 +105,7 @@ const AddNewConnectionBox = () => {
                         <Input type="text" name="url" value={url} onChange={(e)=>setUrl(e.target.value)}/>
                         {url
                         &&
-                            <UrlInputBox>
+                            <UrlInputBox onClick={addhasBeenClicked}>
                                 {`add: ${url}`}
                             </UrlInputBox>
                         }
