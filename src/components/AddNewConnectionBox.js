32fd73c2f2e836c7453ec5760676ecc487f752d8
dio-p <dionempapadop@gmail.com>
2022-11-21 14:18:15 +0000

    working on the input displayeing under the input box

--- src/components/AddNewConnectionBox.js
+++ src/components/AddNewConnectionBox.js
@@ -9,9 +9,50 @@ const AddingConnectionBox = styled.div`
         height: 500px;
     `;
 
-    const FormContainer = styled.form`
-    
-    `;
+const FormContainer = styled.form`
+
+`;
+
+const UrlInputBox = styled.div`
+
+`;
+
+const InputContainer = styled.div`
+    border-width: 2px;
+`;
+
+const Input = styled.input`
+    writing-mode: horizontal-tb !important;
+    font-style: ;
+    font-variant-ligatures: ;
+    font-variant-caps: ;
+    font-variant-numeric: ;
+    font-variant-east-asian: ;
+    font-weight: ;
+    font-stretch: ;
+    font-size: ;
+    font-family: ;
+    text-rendering: auto;
+    color: fieldtext;
+    letter-spacing: normal;
+    word-spacing: normal;
+    line-height: normal;
+    text-transform: none;
+    text-indent: 0px;
+    text-shadow: none;
+    display: inline-block;
+    text-align: start;
+    appearance: auto;
+    -webkit-rtl-ordering: logical;
+    cursor: text;
+    background-color: field;
+    margin: 0em;
+    padding: 1px 2px;
+    border-width: 0px;
+    border-style: inset;
+    border-color: -internal-light-dark(rgb(118, 118, 118), rgb(133, 133, 133));
+    border-image: initial;
+`;
 
 const AddNewConnectionBox = () => {
     const [url, setUrl] = useState("");
@@ -29,7 +70,15 @@ const AddNewConnectionBox = () => {
             <FormContainer>
                 <div>
                     <label htmlFor="">URL</label>
-                    <input type="text" name="url" value={url} onChange={(e)=>setUrl(e.target.value)}/>
+                    <InputContainer>
+                        <Input type="text" name="url" value={url} onChange={(e)=>setUrl(e.target.value)}/>
+                        {url
+                        &&
+                            <UrlInputBox>
+                                {`add: ${url}`}
+                            </UrlInputBox>
+                        }
+                    </InputContainer>
                 </div>
             </FormContainer>
         </AddingConnectionBox>
