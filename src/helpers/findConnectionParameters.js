197a7485f1c69566c32ad7a125e0b8e1d921e44d
Dio Papadopoulos <82506233+Dio-P@users.noreply.github.com>
2022-11-23 19:10:19 +0000

    findConnectionParameters has been tidied up, only thing remaining now is to find a way to get the last modified value

--- src/helpers/findConnectionParameters.js
+++ src/helpers/findConnectionParameters.js
@@ -8,34 +8,13 @@ const findConnectionParameters = async (link) => {
   ))
 
   const lastLinkIndx = linkParts.length -1;
-  
-  const formatedFrag = () => {
-    const linkParts = link
-    .split("/")
-    .filter((fragment) => (
-      fragment !== ""
-    ))
-    const lastLinkIndx = linkParts.length -1;
-    linkParts[lastLinkIndx].split("-").join(" ");
-  }
-  console.log("lastLinkIndx", lastLinkIndx);
-  // fetch(https://github.com/Dio-P/Athena/blob/main/src/components/AddNewConnectionBox.js)
-  // https://paper.dropbox.com/doc/Imposter-syndrome-workshop-ideas--BtYF4rZRzasgUTPjgE7J0osIAg-vIsKjOV2rmXL6H3g39LgZ
-  // https://jira.dev.bbc.co.uk/browse/DPUB-6213
-  // https://confluence.dev.bbc.co.uk/display/NEWSART/Optimo+Articles+Runbook
+
   const findTitle = (source) => {
-    console.log("source!!!!", source);
     if(source==="paper"){
       const lastLinkFragment = linkParts[lastLinkIndx];
       console.log("lastLinkFragment!!!!!", lastLinkFragment);
       return lastLinkFragment.split("--")[0].split("-").join(" ")
-      
     }
-    // if(source==="jira"){
-    //   const lastLinkFragment = link.split("/")[-1];
-    //   return lastLinkFragment.split("--").split("-")
-      
-    // }
     if(source==="confluence"){
       const lastLinkFragment = linkParts[lastLinkIndx];
       return lastLinkFragment.split("+").join(" ")
@@ -59,16 +38,21 @@ const findConnectionParameters = async (link) => {
     if(firstLinkFragment.includes("confluence")){
       return "confluence";
     }
+    if(firstLinkFragment.includes("jira")){
+      return "jira";
+    }
 
   }
   
   
-  console.log("linkParts", linkParts);
   const source = findSource();
-  console.log("source", source);
   const title = await findTitle(source);
-  console.log("title", title);
   return {source, title};
 }
 
 export default findConnectionParameters;
+
+  // fetch(https://github.com/Dio-P/Athena/blob/main/src/components/AddNewConnectionBox.js)
+  // https://paper.dropbox.com/doc/Imposter-syndrome-workshop-ideas--BtYF4rZRzasgUTPjgE7J0osIAg-vIsKjOV2rmXL6H3g39LgZ
+  // https://jira.dev.bbc.co.uk/browse/DPUB-6213
+  // https://confluence.dev.bbc.co.uk/display/NEWSART/Optimo+Articles+Runbook
