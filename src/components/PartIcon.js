039a2f19b1e1401362ab74d897ad4c318332aa79
Dio Papadopoulos <82506233+Dio-P@users.noreply.github.com>
2022-12-10 14:45:43 +0000

    working on the partIcon

--- src/components/PartIcon.js
+++ src/components/PartIcon.js
@@ -1,48 +1,53 @@
 import styled from "@emotion/styled";
+import useCapitaliseFirstLetter from "../hooks/useCapitaliseFirstLetter";
 
 const PartsContainer = styled.div`
     display: flex
 `;
 
-const Icon = styled.button`
-margin: auto;
+const IconButton = styled.button`
 background: none;
 color: inherit;
+
+margin: auto;
+margin-bottom: 20px;
 border: none;
-padding: 0;
+padding: 10px;
 font: inherit;
 cursor: pointer;
 outline: inherit;
-
+min-width: 100px;
+min-height: 170px;
+max-width: 140px;
+max-height: 60px;
+color: white;
 display: flex;
+flex-direction: column;
 align-content: center;
-background-color: #e6056e;
+background-color: #6c98e0;
 width: 90%;
 height: 90%;
 box-shadow: #2b2a28 0.5em 0.5em 0.3em;
 border-radius: 15px 10%;
-margin: 20px;
 font-size: 14px;
 `;
 
-// min-width: 100px;
-// min-height: 50px;
-// max-width: 140px;
-// max-height: 60px;
-
-const PartIcon = ({ part }) => {
+const PartIconButton = ({ part }) => {
+    const partName = useCapitaliseFirstLetter(part.name);
+    const partType = useCapitaliseFirstLetter(part.type);
+    const ghRepo = useCapitaliseFirstLetter(part.ghRepo);
     return (
         <PartsContainer>
-            <Icon>
+            <IconButton>
                 <h4>
-                    { part.name }
+                    { partName }
                 </h4>
-                <p>{ part.type }</p>
-                <p> gitHub: { part.ghRepo }</p>
+                <p>type : { partType }</p>
+                <p> gitHub: { ghRepo }</p>
 
-            </Icon>
+            </IconButton>
         </PartsContainer>
     )
 }
 
-export default PartIcon;
+export default PartIconButton;
