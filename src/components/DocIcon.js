5e307317969a9be74187a8d90fa44a9b215ae6ab
dio-p <dionempapadop@gmail.com>
2022-12-20 16:59:10 +0000

    added some next steps and minor refactorings

--- src/components/DocIcon.js
+++ src/components/DocIcon.js
@@ -3,65 +3,78 @@ import { useEffect, useMemo } from "react";
 import useCapitaliseFirstLetter from "../hooks/useCapitaliseFirstLetter";
 
 const OddIconContainerWrapper = styled.div`
-    display: flex;
-    margin-top: 5px;
-    width: 100%;
-    height: 60px;
-    padding: 3px;
-    color: white;
-    border-radius: 2%;
-    outline: solid black;
-    background-color: #1D4587;
+  display: flex;
+  margin-top: 5px;
+  width: 100%;
+  height: 60px;
+  padding: 3px;
+  color: white;
+  border-radius: 2%;
+  outline: solid black;
+  background-color: #1d4587;
 `;
 
 const EvenIconContainerWrapper = styled.div`
-    display: flex;
-    margin-top: 5px;
-    width: 100%;
-    height: 60px;
-    padding: 3px;
-    color: white;
-    border-radius: 2%;
-    outline: solid black;
-    background-color: green;
+  display: flex;
+  margin-top: 5px;
+  width: 100%;
+  height: 60px;
+  padding: 3px;
+  color: white;
+  border-radius: 2%;
+  outline: solid black;
+  background-color: green;
 `;
 
 const IconContainer = styled.div`
-    display: flex;
-    align-content: center;
-    text-align: center;
-    margin: auto;
+  display: flex;
+  align-content: center;
+  margin: auto;
+`;
+
+const DocSingularInfoContainer = styled.div`
+  text-align: center;
+  margin: 5px;
+
 `;
 
 const DocIcon = ({ doc, iconNu }) => {
-    const {
-        title,
-        url,
-        source,
-        lastModified,
-        concerningParts
-    } = doc;
+  const { title, url, source, lastModified, concerningParts } = doc;
 
-    const docTitle = useCapitaliseFirstLetter(title)
-    const docSource = useCapitaliseFirstLetter(source)
-    const DocIconContainer = (iconNu%2===0)? EvenIconContainerWrapper : OddIconContainerWrapper;
+  const docTitle = useCapitaliseFirstLetter(title);
+  const docSource = useCapitaliseFirstLetter(source);
+  const DocIconContainer =
+    iconNu % 2 === 0 ? EvenIconContainerWrapper : OddIconContainerWrapper;
 
-    // useEffect(() => {
-    //     console.log("iconNu", iconNu);
-    // }, [iconNu])
+  // useEffect(() => {
+  //     console.log("iconNu", iconNu);
+  // }, [iconNu])
 
-    // const count = useMemo(() => {iconNu()}, [iconNu]);
+  // const count = useMemo(() => {iconNu()}, [iconNu]);
 
-    return (
-        <DocIconContainer>
-            <IconContainer>
-                <div>{iconNu}</div>
-                <div> {docTitle} </div>
-            </IconContainer>
-        </DocIconContainer>
-    )
-}
+  return (
+    <DocIconContainer>
+      <IconContainer>
+        <DocSingularInfoContainer>
+            {iconNu}
+        </DocSingularInfoContainer>
+        <DocSingularInfoContainer>
+          <a href={url}>Visit Doc</a>
+        </DocSingularInfoContainer>
+        {/* <DocSingularInfoContainer>
+            {lastModified}
+        </DocSingularInfoContainer> */}
+        {/* <DocSingularInfoContainer>
+            {concerningParts}
+        </DocSingularInfoContainer> */}
+        <DocSingularInfoContainer>
+            {docTitle}
+        </DocSingularInfoContainer>
+      </IconContainer>
+    </DocIconContainer>
+  );
+};
 
 export default DocIcon;
 
-// make that one taking the full page length, and all the info horizontal
+// add the logic that will take the part id's that it concerns and return the titles
