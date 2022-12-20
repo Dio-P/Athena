ac765c50d61f8f4586aa7ad83ef045e0b3f0c5ad
dio-p <dionempapadop@gmail.com>
2022-12-20 14:46:45 +0000

    the data is passing in the singlePartPage as should

--- src/containers/SinglePartPage.js
+++ src/containers/SinglePartPage.js
@@ -1,16 +1,21 @@
+import { useLocation } from 'react-router-dom';
 import styled from "@emotion/styled";
 import DocIcon from "../components/DocIcon";
+import { useEffect } from 'react';
 
-const SinglePartPage = ({ 
-  name, 
-  type, 
-  ghRepo, 
-  folderToBeDisplayedIn,
-  docs
-}) => {
-  const PartPageContainer = styled.div`
+const PartPageContainer = styled.div`
     display: flex
   `;
+
+const SinglePartPage = () => {
+  const {
+    name, 
+    type, 
+    ghRepo, 
+    folderToBeDisplayedIn,
+    docs
+    } = useLocation().state;
+  
   return (
     <PartPageContainer>
       {docs.map((doc)=> (
