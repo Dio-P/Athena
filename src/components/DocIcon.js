7c5ba5a37803fe7b26c4dc28ef8d8082f0503932
dio-p <dionempapadop@gmail.com>
2022-12-20 16:49:35 +0000

    some refactoring on the styling

--- src/components/DocIcon.js
+++ src/components/DocIcon.js
@@ -2,22 +2,35 @@ import styled from "@emotion/styled";
 import { useEffect, useMemo } from "react";
 import useCapitaliseFirstLetter from "../hooks/useCapitaliseFirstLetter";
 
-const OddIconContainer = styled.div`
-display: flex;
-width: 100%;
-height: 60px;
-margin: auto;
-padding: 3px;
-background-color: #1D4587;
+const OddIconContainerWrapper = styled.div`
+    display: flex;
+    margin-top: 5px;
+    width: 100%;
+    height: 60px;
+    padding: 3px;
+    color: white;
+    border-radius: 2%;
+    outline: solid black;
+    background-color: #1D4587;
 `;
 
-const EvenIconContainer = styled.div`
-display: flex;
-width: 100%;
-height: 60px;
-margin: auto;
-padding: 3px;
-background-color: green;
+const EvenIconContainerWrapper = styled.div`
+    display: flex;
+    margin-top: 5px;
+    width: 100%;
+    height: 60px;
+    padding: 3px;
+    color: white;
+    border-radius: 2%;
+    outline: solid black;
+    background-color: green;
+`;
+
+const IconContainer = styled.div`
+    display: flex;
+    align-content: center;
+    text-align: center;
+    margin: auto;
 `;
 
 const DocIcon = ({ doc, iconNu }) => {
@@ -31,7 +44,7 @@ const DocIcon = ({ doc, iconNu }) => {
 
     const docTitle = useCapitaliseFirstLetter(title)
     const docSource = useCapitaliseFirstLetter(source)
-    const DocIconContainer = (iconNu%2===0)? EvenIconContainer : OddIconContainer;
+    const DocIconContainer = (iconNu%2===0)? EvenIconContainerWrapper : OddIconContainerWrapper;
 
     // useEffect(() => {
     //     console.log("iconNu", iconNu);
@@ -41,8 +54,10 @@ const DocIcon = ({ doc, iconNu }) => {
 
     return (
         <DocIconContainer>
-            <div>{iconNu}</div>
-            <div> {docTitle} </div>
+            <IconContainer>
+                <div>{iconNu}</div>
+                <div> {docTitle} </div>
+            </IconContainer>
         </DocIconContainer>
     )
 }
