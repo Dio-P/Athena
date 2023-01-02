07e2ccaa4b661e9f54b2fe800ad8eb1655ebaad7
Dio Papadopoulos <82506233+Dio-P@users.noreply.github.com>
2023-01-02 17:13:03 +0200

    cleaned up moste of the logs and tydied

--- src/hooks/queries/useAppByIdSearch.js
+++ src/hooks/queries/useAppByIdSearch.js
@@ -2,38 +2,6 @@ import { useState, useEffect } from 'react';
 import { useLazyQuery } from "@apollo/client";
 import gql from 'graphql-tag';
 
-// export const SEARCH_APP_BY_ID_QUERY = gql`
-//     query($name: String!){
-//         getAppByName(name: $name){
-//             name
-//             type
-//             gitHubRepo
-//             teams
-//             folders {
-//                 title
-//                 id
-//             }
-//             parts {
-//                 name
-//                 id
-//                 ghRepo
-//                 type
-//                 folderToBeDisplayedIn
-//             }
-//             properties {
-//                 docs {
-//                     title
-//                     url
-//                     id
-//                     source
-//                     lastModified
-//                     concerningParts
-//                 }
-//             }
-//         }
-//     }
-// `;
-
 export const SEARCH_APP_BY_ID_QUERY = gql`
     query($id: ID!) {
         getAppById(id: $id) {
@@ -67,47 +35,29 @@ export const SEARCH_APP_BY_ID_QUERY = gql`
 `;
 
 const useAppByIdSearch = (id) => {
-    const [thisName, setThisName] = useState("");
     const [appToDisplay, setAppToDisplay] = useState("");
 
     const [searchApp, {loading, error, data}] = useLazyQuery(SEARCH_APP_BY_ID_QUERY);
-     useEffect(() => {
-        console.log("name set");
-        setThisName(id)
-     }, [id]);
+ 
      useEffect(() => {
         console.log("app last@@@@", appToDisplay);
-     }, [appToDisplay]);
-     useEffect(() => {
-        console.log("thisName is:", thisName);
-     }, [thisName]);
+     }, [appToDisplay]);///
+ 
     useEffect(() => {
-        console.log("id#@#@#@#", thisName);
-        console.log("type of id#@#@#@#", typeof thisName);
-        if(!thisName){
+        if(!id){
             setAppToDisplay({});
             return;
         }
-        if(thisName){
-            console.log("thisName@@@", thisName);
-            console.log("variables: ", { 
-                variables: 
-                  { id: thisName } 
-                });
-                searchApp({ 
-                    variables: 
-                      { id: thisName } 
-                    })
-        }
+        searchApp({ 
+            variables: 
+                { id: id } 
+            })
          
-    }, [thisName]);
+    }, [id]);
 
     useEffect(() => {
-        console.log("data*!", data);
-        console.log("data.getAppByName*!", data?.getAppById);
         if(data && data.getAppById){
             const newApp = data.getAppById;
-            console.log("newApp", newApp);
             setAppToDisplay({...newApp});
         };
     }, [data]);
@@ -116,6 +66,3 @@ const useAppByIdSearch = (id) => {
 }
 
 export default useAppByIdSearch
-
-// 63b06e8a723b10add8b09aa1
-// 63b06e8a723b10add8b09aa1
