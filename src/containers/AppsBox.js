8d0f6d3ebec5c76784e6233feee74a9e76c44eee
dio-p <dionempapadop@gmail.com>
2023-01-06 10:37:48 +0000

    cleaning up AppsBox

--- src/containers/AppsBox.js
+++ src/containers/AppsBox.js
@@ -1,15 +1,9 @@
-import { useEffect, useState, useMemo } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import ButtonIcon from "../components/ButtonIcon";
 import useCapitaliseFirstLetter from "../hooks/useCapitaliseFirstLetter";
 import AppPage from "./AppPage";
 import styled from "@emotion/styled";
-import useAppByIdSearch from "../hooks/queries/useAppByIdSearch";
-import { v4 as uuidv4 } from "uuid";
-import useValuesFromUrlParams from "../hooks/useValuesFromUrlParams";
-
-const mockPartId1 = uuidv4();
-const mockPartId2 = uuidv4();
 
 const DepartmAppsBoxContainer = styled.div`
   margin-left: 10px;
@@ -32,37 +26,20 @@ const StyledButton = styled.button`
 `;
 
 const AppsBox = ({
-  department,
   teamApps,
   team,
   params,
 }) => {
-  const [returnToThisPage, setReturnToThisPage] = useState(false);
   const [appIdToDisplay, setAppIdToDisplay] = useState("");
-  const [app, setApp] = useState(undefined);
   let [searchParams, setSearchParams] = useSearchParams();
-  // const appIdUrlParam = useMemo(() => {
-  //   console.log("searchParams.get('appId')", searchParams.get('appId'));
-  //   return searchParams.get('appId');
-  // }, [searchParams?.get('appId')!==undefined]);
-  // const appIdUrlParam = searchParams.get('appId');
-  // const [teamParam, appIdParam] = useValuesFromUrlParams()
 
   useEffect(() => {
-    console.log("appBoxParam!!", params);
     if(!appIdToDisplay && params.appId){
-      // console.log("setting params to appIdUrlParam");
       setAppIdToDisplay(params.appId);
     }else {
-      // console.log("setting app id to empty string ");
       setAppIdToDisplay("");
     }
   }, []);
-
-  // useEffect(() => {
-  //   console.log("params#@#@##@", teamParam, appIdParam);
-  
-  // }, [teamParam, appIdParam]);
 
   const clickingToHere = () => {
     setAppIdToDisplay("");
