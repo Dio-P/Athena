3ae418bafbf878d66a112a0a73535d9b3d754afa
dio-p <dionempapadop@gmail.com>
2023-01-20 17:32:20 +0000

    this is going well, but the initial value before clicking on the folder button is again an empty stringg

--- src/hooks/useComposeNewDocSearchParam.js
+++ src/hooks/useComposeNewDocSearchParam.js
@@ -1,31 +1,40 @@
 import { useEffect, useState } from "react";
 
+const validateToStringOrNone = (newValues) =>
+newValues.lenght > 0 ? newValues.join("~") : "NONE";
+
 const composeNewFolderSearchParam = (newFolderObj) => {
   const newFolderValues = Object.values(newFolderObj);
-  console.log("newFolderValues", newFolderValues);
-  const newFolderValuesToString = newFolderValues.join("~");
-  console.log("newFolderValuesToString@@", newFolderValuesToString);
-  return newFolderValuesToString;
+  return validateToStringOrNone(newFolderValues);
 };
 
-const useComposeNewDocSearchParam = (newFolderObj) => {
-  const [docSearchParams, setDocSearchParams] = useState(undefined)
-  const [newUserInput, setNewUserInput] = useState(undefined)
+const composeNewPartSearchParam = (newPartObj) => {
+  const newPartValues = Object.values(newPartObj);
+  return validateToStringOrNone(newPartValues)
+};
 
-  let newFolderParams = newFolderObj && composeNewFolderSearchParam(newFolderObj);
-  // let newFolderParams = composeNewFolderSearchParam(newFolderObj) || undefined;
-  useEffect(() => {
-    setNewUserInput({newFolder: newFolderParams, test: "test"})
-  }, [newFolderParams]);
+const useComposeNewDocSearchParam = (newFolderObj, newPartObj) => {
+  const [docSearchParams, setDocSearchParams] = useState(undefined);
+  const [newUserInput, setNewUserInput] = useState(undefined);
+
+  let newFolderParams =
+    newFolderObj && composeNewFolderSearchParam(newFolderObj);
+  let newPartParams = newPartObj && composeNewPartSearchParam(newPartObj);
 
   useEffect(() => {
-    if(newUserInput){
+    console.log("newFolderParams@@", newFolderParams);
+    console.log("newPartParams@@", newPartParams);
+    setNewUserInput([newFolderParams, newPartParams]);
+  }, [newFolderParams, newPartParams]);
+
+  useEffect(() => {
+    if (newUserInput) {
       console.log("newUserInput££", newUserInput);
-      const allNewFieldsjoinedString = Object.values(newUserInput).join("|")
-      setDocSearchParams(allNewFieldsjoinedString) 
+      const allNewFieldsjoinedString = Object.values(newUserInput).join("|");
+      setDocSearchParams(allNewFieldsjoinedString);
     }
-  }, [newUserInput])
-  return [docSearchParams]
+  }, [newUserInput]);
+  return [docSearchParams];
 };
 
 export default useComposeNewDocSearchParam;
