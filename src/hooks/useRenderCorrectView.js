bba8353a6669a24dc42afd3f6df9be211ddcdd32
Dio Papadopoulos <82506233+Dio-P@users.noreply.github.com>
2023-01-28 09:59:13 +0000

    added useMemo in render custom hook

--- src/hooks/useRenderCorrectView.js
+++ src/hooks/useRenderCorrectView.js
@@ -1,13 +1,19 @@
+import { useMemo } from "react";
 import { useEffect, useState } from "react";
 
 const useRenderCorrectView = (loading, error, data, view) => {
   const [renderingView, setRenderingView] = useState(undefined);
 
+  const loadingMemo = useMemo(() => loading, [loading]);
+  const errorMemo = useMemo(() => error, [error]);
+  const dataMemo = useMemo(() => data, [data]);
+  const viewMemo = useMemo(() => view, [view]);
+
   useEffect(() => {
-    if (loading) {
+    if (loadingMemo) {
       setRenderingView(<h3>Loading...</h3>);
     }
-    if (error) {
+    if (errorMemo) {
       setRenderingView(
         <p>
           I am sad to say that the following error was just reported :
@@ -15,9 +21,9 @@ const useRenderCorrectView = (loading, error, data, view) => {
         </p>
       );
     }
-    if (data) {
-      console.log('data£££', data);
-      setRenderingView(view);
+    if (dataMemo) {
+      console.log('data£££', dataMemo);
+      setRenderingView(viewMemo);
     }
   }, [loading, error, data])
   
