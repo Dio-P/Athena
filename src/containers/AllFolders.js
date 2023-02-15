ce10719e1ca0b253da04256ae2eff5531262af17
dio-p <dionempapadop@gmail.com>
2023-02-15 15:03:42 +0000

    cleaned allFolders a bit up. next step to create the pop up to appear when adding a new folder

--- src/containers/AllFolders.js
+++ src/containers/AllFolders.js
@@ -1,47 +1,21 @@
-import AddNewFolderInputContainer from "./AddNewFolderInputContainer";
-import PopulateButtonUnits from "./PopulateButtonUnits";
 import DropDown from "../components/DropDown";
-import useParamsHelper from "../hooks/useParamsHelper";
 
 const AllFolders = ({ 
   allPreexistingFolders, 
   allNewFolders, 
   folderInfoToState, 
-  newFolderName, 
-  addNewFolderAndClear, 
-  newInputTitle, 
-  onClickingFolder,
-  // clickingToAddNewFolder,
   folderOfNewPart,
 }) => {
 
-  const {
-    params: {
-      folderDdOpen
-    }
-  } = useParamsHelper();
-
   return (
-    // folderDdOpen && 
-    // (
       <>
         <DropDown
           preexistingFolders={allPreexistingFolders}
           newFolders={allNewFolders}
           onClickFunction={folderInfoToState}
-          // clickingToAddNewFolder={clickingToAddNewFolder}
           folderOfNewPart={folderOfNewPart}
         />
       </>
-    // ) 
-  //   : (
-  //     <AddNewFolderInputContainer
-  //       newFolderName={newFolderName}
-  //       addNewFolderAndClear={addNewFolderAndClear}
-  //       newInputTitle={newInputTitle}
-  //       onClickingFolder={onClickingFolder}
-  //     />
-  //   )
   )
 }
 
