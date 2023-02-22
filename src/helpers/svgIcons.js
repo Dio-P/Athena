0a134b7d900bdda8e5f63f1740bd39626afd0915
dio-p <dionempapadop@gmail.com>
2023-02-22 17:52:34 +0000

    aria-hidden on the svgs

--- src/helpers/svgIcons.js
+++ src/helpers/svgIcons.js
@@ -3,6 +3,7 @@ export const arrowDownIcon = (
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 32 32"
     aria-label="arrow-down"
+    aria-hidden
   >
     <path d="M26.7 12.6L16 23.2 5.3 12.6V8.8h21.4v3.8z"></path>
   </svg>
@@ -13,6 +14,7 @@ export const arrowUpIcon = (
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 32 32"
     aria-label="arrow-up"
+    aria-hidden
   >
     <path d="M26.7 19.4L16 8.8 5.3 19.4v3.7h21.4v-3.7z"></path>
   </svg>
@@ -23,6 +25,7 @@ export const magnifyingGlassIcon = (
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 32 32"
     aria-label="search"
+    aria-hidden
   >
     <path d="M13 24.3c6.5 0 11.7-5.1 11.7-11.7S19.6 1 13 1 1.4 6.1 1.4 12.7 6.5 24.3 13 24.3zm0-2.6c-5.1 0-9-3.9-9-9.1s3.9-9.1 9-9.1 9 3.9 9 9.1-3.9 9.1-9 9.1zm5.8.4l8.9 8.9 2.9-2.9-8.9-8.9-2.9 2.9z"></path>
   </svg>
@@ -34,6 +37,7 @@ export const warningIcon = (
     viewBox="0 0 32 32"
     aria-label="warning"
     fill="red"
+    aria-hidden
   >
     <path d="M3.1 30.2h25.8c1.6 0 2.3-.8 2.3-1.9 0-.8-.3-1.4-1-2.9L18.6 3.8c-.8-1.5-1.5-2-2.6-2s-1.8.5-2.6 2L1.8 25.4C1 26.9.7 27.5.7 28.3c.1 1.1.8 1.9 2.4 1.9zM16 26.9c-1.6 0-2.4-1-2.4-2.1 0-1.2.8-2.1 2.4-2.1s2.4 1 2.4 2.1-.8 2.1-2.4 2.1zm-1.7-6.7l-.8-10.6h5l-.8 10.6h-3.4z"></path>
   </svg>
@@ -55,6 +59,7 @@ export const tick = (
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 32 32"
     aria-label="confirm"
+    aria-hidden
   >
     <path
       fill="#1ee685"
