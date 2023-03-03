00abb227984f75b5ecaec7964aede8d0e1aa4aeb
dio-p <dionempapadop@gmail.com>
2023-03-03 17:21:45 +0000

    working on the edit style

--- src/containers/EditAppContainer.js
+++ src/containers/EditAppContainer.js
@@ -3,10 +3,13 @@ import GenericButtonIcon from "../components/GenericButtonIcon";
 import { deleteIcon } from "../helpers/svgIcons";
 import ManageAppDetails from "../components/ManageAppDetails";
 import ManageFoldersDetails from "../components/ManageFoldersDetails";
+import styleVariables from '../styleVariables';
 
 const EditAppContainerWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  height: 100%;
+  background-color: ${styleVariables.colours.primaryLight}
 `;
 
 const Body = styled.div`
